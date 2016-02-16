482c42cd5396b0a3f270fc9f8c4cab91323d37eb
jojotjebaby <joris.hart14@gmail.com>
2016-02-16 18:14:32 +0100

    #323
    Old deployement method

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -7,8 +7,7 @@ set :user, "joris" # Le nom d’utilisateur du serveur distant
 set :repository, "git@github.com:jojotjebaby/Bestfootball.git" # L’URL de votre repository
 set :branch, "master" # La branche Git, utile si vous pushez vos releases de prod sur une branche particulière
 set :scm, :git # SVN ? Haha, vous plaisantez j’espère :-)
-set :deploy_via, :rsync_with_remote_cache # Ils y a plusieurs méthodes de déploiements, nous utilisons la méthode de copy
-set :rsync_options, "-az --delete --progress"
+set :deploy_via, :copy # Ils y a plusieurs méthodes de déploiements, nous utilisons la méthode de copy
 
 set :model_manager, "doctrine" # ORM
 
@@ -20,8 +19,6 @@ role :app, domain, :primary => true
 
 set :use_sudo, true
 set :keep_releases, 3 # Le nombre de releases à garder après un déploiement réussi
-
-
 
 ## Symfony2
 set :shared_files, ["app/config/parameters.yml"] # Les fichiers à conserver entre chaque déploiement
