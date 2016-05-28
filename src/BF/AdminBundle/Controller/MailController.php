ef12803cc204a806b9c58e2e73f244d137a2195b
jojotjebaby <joris.hart14@gmail.com>
2016-05-28 18:53:23 +0200

    #687
    Adding the newsletter feature !

---
+++ src/BF/AdminBundle/Controller/MailController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace BF\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class MailController extends Controller
+{
+    public function newsletterAction(request $request)
+    {
+      //make a form to send a newsletter
+      $form = $this->createFormBuilder()
+            ->add('message', 'textarea')
+            ->add('challengeMessage', 'textarea')
+            ->add('subject', 'text')
+            ->add('challenge', 'entity', array(
+                  'class' => 'BFSiteBundle:Challenge',
+                  'choice_label' => 'titleFR',
+              ))
+            ->add('save', 'submit', array('label' => 'Create Task'))
+            ->getForm();
+
+
+
+          $form->handleRequest($request);
+
+          if ($form->isSubmitted() && $form->isValid()) {
+
+            //getting the last 3 videos
+            $listVideos = $this->getDoctrine()->getManager()->getRepository('BFSiteBundle:Video')->findBy(array(),array('date' => 'desc'),2,0);
+            $listUsers = $this->getDoctrine()->getManager()->getRepository('BFUserBundle:User')->findBy(array('enabled' => 1));
+
+            $data = $form->getData();
+
+            $challenge = $data['challenge'];
+            $challengeMessage = $data['challengeMessage'];
+            $message = $data['message'];
+            $subject = $data['subject'];
+
+
+              // Send the mail to all the users
+              foreach ($listUsers as $user) {
+              //send the message to the user.
+                  $message = \Swift_Message::newInstance()
+                      ->setSubject($subject)
+                      ->setFrom('noreply@bestfootball.fr')
+                      ->setTo($user->getEmailCanonical())
+                      ->setBody(
+                        $this->renderView(
+                          // app/Resources/views/Emails/newsletter.html.twig
+                              'Emails/newsletter.html.twig',array(
+                                'subject' => $subject,
+                                'message' => $message,
+                                'user' => $user,
+                                'listVideos' => $listVideos,
+                                'challenge' => $challenge,
+                                'challengeMessage' => $challengeMessage
+                              )
+                        ),
+                        'text/html'
+                  );
+                $this->get('mailer')->send($message);
+              }
+
+              $request->getSession()->getFlashBag()->add('notice', 'The mail has been send!');
+              return $this->redirect($this->generateUrl('bf_site_admin'));
+          }
+
+
+    
+
+      return $this->render('BFAdminBundle:Mail:newsletter.html.twig',array(
+        'form' => $form->createView(),
+      ));
+
+
+
+
+
+
+
+
+
+
+    	$listVideos = $this->getDoctrine()->getManager()->getRepository('BFSiteBundle:Video')->findall();
+	    return $this->render('BFAdminBundle:Video:index.html.twig',array(
+	    	'listVideos' => $listVideos,
+	    	));
+    }
+}
