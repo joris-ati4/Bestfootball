3c714162f8250932be538363d3e394b306bd3a2e
jojotjebaby <joris.hart14@gmail.com>
2016-06-08 21:39:48 +0200

    #749
    Test

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -40,12 +40,12 @@ logger.level = Logger::MAX_LEVEL
 default_run_options[:pty] = true
 
 # Et enfin, si jamais vous rencontrez des erreurs de permissions, vous pouvez rajouter ces lignes suivantes :
-#after "deploy:finalize_update" do
-#run "sudo chown -R joris:www-data #{latest_release}"
-#run "sudo chown -R joris:www-data #{latest_release}/app/spool"
-#run "sudo chown -R joris:www-data #{latest_release}/app/logs/dev.log"
-#run "sudo chown -R joris:www-data #{latest_release}/app/logs/prod.log"
-#run "sudo chmod -R 777 #{latest_release}/#{cache_path}"
-#run "sudo chmod -R 777 #{latest_release}/#{log_path}"
-#run "sudo chmod -R 777 #{latest_release}/#{log_path}"
-#end
+after "deploy:finalize_update" do
+run "sudo chown -R joris:www-data #{latest_release}"
+run "sudo chown -R joris:www-data #{latest_release}/app/spool"
+run "sudo chown -R joris:www-data #{latest_release}/app/logs/dev.log"
+run "sudo chown -R joris:www-data #{latest_release}/app/logs/prod.log"
+run "sudo chmod -R 777 #{latest_release}/#{cache_path}"
+run "sudo chmod -R 777 #{latest_release}/#{log_path}"
+run "sudo chmod -R 777 #{latest_release}/#{log_path}"
+end
